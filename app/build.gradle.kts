3a133d91e763f74846884995d2e30a52f0b15990
Alejandro Lagos <flagos.rd@gmail.com>
2024-08-20 16:42:28 -0600

    Removed commented code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,4 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
-
-   // testImplementation(libs.junit)
-   // androidTestImplementation(libs.androidx.junit)
-   // androidTestImplementation(libs.androidx.espresso.core)
 }
